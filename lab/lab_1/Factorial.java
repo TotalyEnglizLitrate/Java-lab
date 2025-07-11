adb4e7c5139ea2a4e2a5101199c0228ce249f415
Narendra S <narendra24110064@snuchennai.edu.in>
2025-07-11 11:19:37 +0530

    remove recursion - factorial

--- lab/lab_1/Factorial.java
+++ lab/lab_1/Factorial.java
@@ -4,31 +4,33 @@ import java.util.Scanner;
 
 public class Factorial {
     private Long getNum(Scanner console) {
-        System.out.print("Enter number for which you want factorial: ");
-
-        while(!console.hasNextLong()) {
-            System.out.println("Invalid input! please try again");
+        Long num = -1L;
+        while (num < 0) {
             System.out.print("Enter number for which you want factorial: ");
+            if (console.hasNextLong()) {
+                num = console.nextLong();
+                if (num < 0) {
+                    System.out.println("Invalid input! please try again");
+                }
+            } else {
+                System.out.println("Invalid input! please try again");
+                console.next();
+            }
         }
-
-        Long num = console.nextLong();
-
-        if (num < 0) {
-            System.out.println("Invalid input! please try again");
-            return this.getNum(console);
-        }
-
         return num;
     }
 
     private Long factorial(Long n) {
-        if (n == 0 || n == 1) {
-            return (long)1;
+        Long result = 1L;
+        for (Long i = 2L; i <= n; i++) {
+            result *= i;
+            if (result <= 0) {
+                return 0L;
+            }
         }
-
-        return this.factorial(n - 1) * n;
-
+        return result;
     }
+
     public static void main(String[] args) {
         Factorial fact = new Factorial();
         Scanner console = new Scanner(System.in);
@@ -39,7 +41,7 @@ public class Factorial {
         Long num_fact = fact.factorial(num);
         if (num_fact == 0) {
             System.out.println("Factorial too large to represent");
-        }else {
+        } else {
             System.out.printf("%d! = %d\n", num, num_fact);
         }
     }
